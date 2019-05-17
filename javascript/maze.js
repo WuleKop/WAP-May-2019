130b5332e94b566bbe5c5d552ed6cdc3bf1e4684
Wuletaw Adane Tefera <wule2000@gmail.com>
2019-05-17 10:20:09 -0500

    Final Maze

--- javascript/maze.js
+++ javascript/maze.js
@@ -1,26 +1,37 @@
 /*global $:false*/
 ($(document).ready(function() {
     "use strict";
+    let i=false;
+    let j=false;
     $("#start").click(function () {
-        let i=0;
         $("#maze").mouseleave(function () {
-            $(".boundary").addClass("youlose");
-            $("#status").text("Sorry, You are Cheating!");
-            i=1;
+            if(!j) {
+                $(".boundary").addClass("youlose");
+                $("#status").text("Sorry, You are Cheating!");
+                i = true;
+                j=true;
+            }
+
         });
         $(".boundary").mouseover(function () {
-            $(".boundary").addClass("youlose");
-            $("#status").text("Sorry, You Lost!");
-            i=1;
+            if(!j) {
+                $(".boundary").addClass("youlose");
+                $("#status").text("Sorry, You Lost!");
+                i = true;
+                j=true;
+            }
         });
         $("#end").click(function () {
-            if(i===0) {
+            if(!i) {
                 $("#status").text("You Win!" );
+                j=true;
             }
         });
         $("#start").click(function () {
             $(".boundary").removeClass("youlose");
             $("#status").text("Here We go Again!");
+            i=false;
+            j=false;
         });
     });
 })());
